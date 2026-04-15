06803c8574be117b513325dd213b0d251e921936
Naritoshi Iwano <cwq06831@gmail.com>
2026-04-15 23:04:07 +0900

    Fix CORS: add PUT and DELETE to allowed methods

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -9,7 +9,7 @@ const KV_LAST_UPDATED = 'last_updated';
 // CORS（PagesのドメインとローカルdevのためALL許可）
 const CORS_HEADERS = {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type',
 };
 
